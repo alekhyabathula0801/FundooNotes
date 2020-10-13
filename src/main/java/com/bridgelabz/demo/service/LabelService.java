ff45758832705f10be9fc16222b0f8ea15140640
arun kumar <alekhya0801@gmail.com>
2020-10-13 15:35:40 +0530

    Refactored : optimized code

--- src/main/java/com/bridgelabz/demo/service/LabelService.java
+++ src/main/java/com/bridgelabz/demo/service/LabelService.java
@@ -2,10 +2,8 @@ package com.bridgelabz.demo.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -42,114 +40,67 @@ public class LabelService {
 
 	public ResponseEntity<Response> addLabel(Label label, String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			User user = userRepository.findById(userId).get();
-			label.setUser(user);
-			List<Label> allLabels = labelRepository.findAllLabelsByUserId(userId);
-			if (allLabels.contains(label))
-				return new ResponseEntity<Response>(userService.getResponse(Message.LABEL_NAME_EXISTS, null, 409),
-						HttpStatus.CONFLICT);
-			Label labels = labelRepository.save(label);
-			return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, labels, 200),
-					HttpStatus.OK);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.USER_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new FundooNotesException(Message.USER_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		label.setUser(user);
+		List<Label> allLabels = labelRepository.findAllLabelsByUserId(userId);
+		if (allLabels.contains(label))
+			return new ResponseEntity<Response>(userService.getResponse(Message.LABEL_NAME_EXISTS, null, 409),
+					HttpStatus.CONFLICT);
+		Label labels = labelRepository.save(label);
+		return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, labels, 200), HttpStatus.OK);
 	}
 
 	public ResponseEntity<Response> updateLabel(Label label, String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			Label labels = labelRepository.findById(label.getLabelId()).get();
-			if (labels.getUser().getUserId() == userId) {
-				labels.setModifiedDate(LocalDateTime.now());
-				labels.setLabelName(label.getLabelName());
-				labels = labelRepository.save(labels);
-				return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, labels, 200),
-						HttpStatus.OK);
-			}
-			return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
-					HttpStatus.BAD_REQUEST);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.LABEL_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
+		Label labels = labelRepository.findById(label.getLabelId())
+				.orElseThrow(() -> new FundooNotesException(Message.LABEL_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		if (labels.getUser().getUserId() == userId) {
+			labels.setModifiedDate(LocalDateTime.now());
+			labels.setLabelName(label.getLabelName());
+			labels = labelRepository.save(labels);
+			return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, labels, 200),
+					HttpStatus.OK);
 		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
+				HttpStatus.BAD_REQUEST);
 	}
 
 	public ResponseEntity<Response> deleteLabel(Long labelId, String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			Label label = labelRepository.findById(labelId).get();
-			if (label.getUser().getUserId().equals(userId)) {
-				labelRepository.deleteById(labelId);
-				return new ResponseEntity<Response>(
-						userService.getResponse(Message.SUCCESSFUL, Message.LABEL_DELETED_SUCCESFULLY, 200),
-						HttpStatus.OK);
-			}
-			return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
-					HttpStatus.BAD_REQUEST);
-		} catch (EmptyResultDataAccessException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.LABEL_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
+		Label label = labelRepository.findById(labelId)
+				.orElseThrow(() -> new FundooNotesException(Message.LABEL_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		if (label.getUser().getUserId().equals(userId)) {
+			labelRepository.deleteById(labelId);
+			return new ResponseEntity<Response>(
+					userService.getResponse(Message.SUCCESSFUL, Message.LABEL_DELETED_SUCCESFULLY, 200), HttpStatus.OK);
 		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
+				HttpStatus.BAD_REQUEST);
 	}
 
 	public ResponseEntity<Response> getAllLabelsByUserId(String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			userRepository.findById(userId).get();
-			List<Label> allLabels = labelRepository.findAllLabelsByUserId(userId);
-			return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, allLabels, 200),
-					HttpStatus.OK);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.USER_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		userRepository.findById(userId)
+				.orElseThrow(() -> new FundooNotesException(Message.USER_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		List<Label> allLabels = labelRepository.findAllLabelsByUserId(userId);
+		return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, allLabels, 200), HttpStatus.OK);
 	}
 
 	public ResponseEntity<Response> mapLabelAndNotes(String token, Long labelId, Long noteId) {
-		Label label;
-		try {
-			label = labelRepository.findById(labelId).get();
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.LABEL_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		}
-		try {
-			Note note = noteRepository.findById(noteId).get();
-			if (!note.getUser().getUserId().equals(label.getUser().getUserId()))
-				return new ResponseEntity<Response>(
-						userService.getResponse(Message.USER_ID_OF_LABEL_DOESNT_MATCH_WITH_NOTE_USER_ID, null, 404),
-						HttpStatus.BAD_REQUEST);
-			note.getLabel().add(label);
-			note.setModifiedDate(LocalDateTime.now());
-			note = noteRepository.save(note);
-			return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, note, 200), HttpStatus.OK);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.NOTE_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		Label label = labelRepository.findById(labelId)
+				.orElseThrow(() -> new FundooNotesException(Message.LABEL_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+
+		Note note = noteRepository.findById(noteId)
+				.orElseThrow(() -> new FundooNotesException(Message.NOTE_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		if (!note.getUser().getUserId().equals(label.getUser().getUserId()))
+			return new ResponseEntity<Response>(
+					userService.getResponse(Message.USER_ID_OF_LABEL_DOESNT_MATCH_WITH_NOTE_USER_ID, null, 404),
+					HttpStatus.BAD_REQUEST);
+		note.getLabel().add(label);
+		note.setModifiedDate(LocalDateTime.now());
+		note = noteRepository.save(note);
+		return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, note, 200), HttpStatus.OK);
 	}
 
 	public ResponseEntity<Response> removeLabelFromNote(String token, Long labelId, Long noteId) {
--- src/main/java/com/bridgelabz/demo/service/NoteService.java
+++ src/main/java/com/bridgelabz/demo/service/NoteService.java
@@ -4,11 +4,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -41,41 +39,25 @@ public class NoteService {
 
 	public ResponseEntity<Response> addNote(Note note, String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			User user = userRepository.findById(userId).get();
-			note.setUser(user);
-			Note noteDetails = noteRepository.save(note);
-			return new ResponseEntity<Response>(
-					userService.getResponse(Message.NOTE_ADDED_SUCCESFULLY, noteDetails, 200), HttpStatus.OK);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.USER_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new FundooNotesException(Message.USER_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		note.setUser(user);
+		Note noteDetails = noteRepository.save(note);
+		return new ResponseEntity<Response>(userService.getResponse(Message.NOTE_ADDED_SUCCESFULLY, noteDetails, 200),
+				HttpStatus.OK);
 	}
 
 	public ResponseEntity<Response> getAllNotesByUserId(String token, boolean isTrash) {
 		List<Note> availableNotes = new ArrayList<Note>();
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			Optional<User> user = userRepository.findById(userId);
-			List<Note> collaboratedNote = user.get().getCollaboratedNote().stream().collect(Collectors.toList());
-			availableNotes = noteRepository.findAllNotesByUserId(userId, Boolean.valueOf(false),
-					Boolean.valueOf(isTrash));
-			availableNotes.addAll(collaboratedNote);
-			return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, availableNotes, 200),
-					HttpStatus.OK);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.USER_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new FundooNotesException(Message.USER_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		List<Note> collaboratedNote = user.getCollaboratedNote().stream().collect(Collectors.toList());
+		availableNotes = noteRepository.findAllNotesByUserId(userId, Boolean.valueOf(false), Boolean.valueOf(isTrash));
+		System.out.print("\n" + availableNotes);
+		availableNotes.addAll(collaboratedNote);
+		return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, availableNotes, 200),
+				HttpStatus.OK);
 	}
 
 	public ResponseEntity<Response> getAllArchiveNotesByUserId(String token) {
@@ -96,71 +78,45 @@ public class NoteService {
 
 	public ResponseEntity<Response> setIsTrash(Long noteId, boolean isTrash, String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			Note note = noteRepository.findById(noteId).get();
-			if (note.getUser().getUserId().equals(userId)) {
-				note.setTrash(isTrash);
-				note.setModifiedDate(LocalDateTime.now());
-				Note notes = noteRepository.save(note);
-				return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, notes, 200),
-						HttpStatus.OK);
-			}
-			return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
-					HttpStatus.BAD_REQUEST);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.NOTE_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
+		Note note = noteRepository.findById(noteId)
+				.orElseThrow(() -> new FundooNotesException(Message.NOTE_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		if (note.getUser().getUserId().equals(userId)) {
+			note.setTrash(isTrash);
+			note.setModifiedDate(LocalDateTime.now());
+			Note notes = noteRepository.save(note);
+			return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, notes, 200), HttpStatus.OK);
 		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
+				HttpStatus.BAD_REQUEST);
 	}
 
 	public ResponseEntity<Response> deleteNote(Long noteId, String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			Note note = noteRepository.findById(noteId).get();
-			if (note.getUser().getUserId().equals(userId)) {
-				noteRepository.deleteById(noteId);
-				return new ResponseEntity<Response>(
-						userService.getResponse(Message.SUCCESSFUL, Message.NOTE_DELETED_SUCCESFULLY, 200),
-						HttpStatus.OK);
-			}
-			return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
-					HttpStatus.BAD_REQUEST);
-		} catch (EmptyResultDataAccessException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.NOTE_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
+		Note note = noteRepository.findById(noteId)
+				.orElseThrow(() -> new FundooNotesException(Message.NOTE_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		if (note.getUser().getUserId().equals(userId)) {
+			noteRepository.deleteById(noteId);
+			return new ResponseEntity<Response>(
+					userService.getResponse(Message.SUCCESSFUL, Message.NOTE_DELETED_SUCCESFULLY, 200), HttpStatus.OK);
 		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
+				HttpStatus.BAD_REQUEST);
 	}
 
 	public ResponseEntity<Response> updateNote(Note note, String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			Note notes = noteRepository.findById(note.getNoteId()).get();
-			if (notes.getUser().getUserId().equals(userId)) {
-				notes.setTitle(note.getTitle());
-				notes.setDescription(note.getDescription());
-				notes.setModifiedDate(LocalDateTime.now());
-				Note noteDetails = noteRepository.save(notes);
-				return new ResponseEntity<Response>(
-						userService.getResponse(Message.NOTE_UPDATED_SUCCESFULLY, noteDetails, 200), HttpStatus.OK);
-			}
-			return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
-					HttpStatus.BAD_REQUEST);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.NOTE_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
+		Note notes = noteRepository.findById(note.getNoteId())
+				.orElseThrow(() -> new FundooNotesException(Message.NOTE_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		if (notes.getUser().getUserId().equals(userId)) {
+			notes.setTitle(note.getTitle());
+			notes.setDescription(note.getDescription());
+			notes.setModifiedDate(LocalDateTime.now());
+			Note noteDetails = noteRepository.save(notes);
+			return new ResponseEntity<Response>(
+					userService.getResponse(Message.NOTE_UPDATED_SUCCESFULLY, noteDetails, 200), HttpStatus.OK);
 		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
+				HttpStatus.BAD_REQUEST);
 	}
 
 	public ResponseEntity<Response> updateColor(Long noteId, String color, String token) {
@@ -216,28 +172,21 @@ public class NoteService {
 
 	public ResponseEntity<Response> addCollabrator(Collabrator collabrator, String token) {
 		Long userId = UserToken.getUserIdFromToken(token);
-		try {
-			User user = userRepository.findByEmail(collabrator.getCollabratorEmail());
-			Note note = noteRepository.findById(collabrator.getNoteId()).get();
-			if (userId == note.getUser().getUserId()) {
-				user.getCollaboratedNote().add(note);
-				note.getCollaboratedUser().add(user);
-				note.setModifiedDate(LocalDateTime.now());
-				userRepository.save(user);
-				note = noteRepository.save(note);
-				return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, note, 200),
-						HttpStatus.OK);
-			}
-			return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
-					HttpStatus.BAD_REQUEST);
-		} catch (NoSuchElementException e) {
-			return new ResponseEntity<Response>(userService.getResponse(Message.NOTE_ID_DOESNOT_EXISTS, null, 404),
-					HttpStatus.NOT_FOUND);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
+		User user = userRepository.findByEmail(collabrator.getCollabratorEmail());
+		if (user == null)
+			throw new FundooNotesException(Message.EMAIL_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST);
+		Note note = noteRepository.findById(collabrator.getNoteId())
+				.orElseThrow(() -> new FundooNotesException(Message.NOTE_ID_DOESNOT_EXISTS, HttpStatus.BAD_REQUEST));
+		if (userId == note.getUser().getUserId()) {
+			user.getCollaboratedNote().add(note);
+			note.getCollaboratedUser().add(user);
+			note.setModifiedDate(LocalDateTime.now());
+			userRepository.save(user);
+			note = noteRepository.save(note);
+			return new ResponseEntity<Response>(userService.getResponse(Message.SUCCESSFUL, note, 200), HttpStatus.OK);
 		}
-		return new ResponseEntity<Response>(userService.getResponse(Message.TRY_AGAIN_LATER, null, 500),
-				HttpStatus.SERVICE_UNAVAILABLE);
+		return new ResponseEntity<Response>(userService.getResponse(Message.INVALID_USER_ID, null, 404),
+				HttpStatus.BAD_REQUEST);
 	}
 
 	public ResponseEntity<Response> removeCollabrator(Long noteId, String email, String token) {
